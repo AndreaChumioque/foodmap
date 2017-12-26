901f2891925c02a46d151640cff3b5d42ac29fea
andrea-isabel <andrea19.93.15@gmail.com>
2017-12-25 21:05:28 -0500

    splash screen - prueba redireccionamiento a home

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var splash = $('#splash');
+  var searchBar = $('#search-bar');
+
+  splash.fadeIn('fast');
+
+  setTimeout(fadeSplash, 2000);
+  searchBar.keyup(searchFilter);
+
+  function fadeSplash() {
+    splash.fadeOut();
+    window.location.href = '../views/home.html';
+  }
+
+  function searchFilter(event) {
+    var searchWords = searchBar.val();
+    $('.collection').hide();
+    $('.collection li').each(function() {
+      var match = $(this).text();
+      if (match.indexOf(searchWords) !== -1) {
+        $(this).fadeIn('fast');
+      }
+    });
+  }
+});
